bccc304424220e8ee8337e6a68ac31b0aef5b962
Guibsb90 <122750380+Guibsb90@users.noreply.github.com>
2024-07-22 12:02:57 -0300

    primeiro commit

---
+++ teste.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplica(num1, num2) {
+    return num1 * num2;
+}
+console.log(multiplica(3, 4));
+function saudacao(nome) {
+    return console.log(`Olá, ${nome}`);
+}
+saudacao("Joao");
---
+++ teste.ts
@@ -0,0 +1,11 @@
+function multiplica(num1: number, num2: number){
+  return num1 * num2
+}
+
+console.log(multiplica(3, 4));
+
+function saudacao(nome: string){
+  return console.log(`Olá, ${nome}`)
+}
+
+saudacao("Joao");
